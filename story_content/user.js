dbaa5df24b6c23a7a51a31c880cdfd520fc2219d
Iván Vargas O <ivanvargas.va@gmail.com>
2023-04-14 20:23:17 -0500

    subida muestra

---
+++ story_content/user.js
@@ -0,0 +1,16 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5wZm60UWvI1":
+        Script1();
+        break;
+  }
+}
+
+function Script1()
+{
+  stopAudio();
+playAudio('tech-house-vibes-130.mp3')
+}
+
